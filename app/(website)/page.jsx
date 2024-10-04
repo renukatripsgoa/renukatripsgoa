25d65734770122c34934fe8e9f067315f56f34b3
renukatripsgoa <renukatripsgoa44@gmail.com>
2024-10-04 11:29:32 +0530

    Update pager

--- app/(website)/page.jsx
+++ app/(website)/page.jsx
@@ -26,13 +26,7 @@ const Home = () => {
         <Card onSelectTitle={handleCardSelect} />
               <div className="flex h-44 w-full shadow-md rounded-xl border object-cover">
 
-        <iframe
-          className="w-full h-full overflow-hidden object-cover"
-          src={`https://maps.app.goo.gl/6EYAzSAonVQ3yN1n8`}
-          allowFullScreen=""
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        />
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3843.9884543128114!2d73.75903857508901!3d15.538749653420867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbfc14acb1c97c9%3A0x15975bbbeeec0083!2sRenuka%20Trips%20Goa!5e0!3m2!1sen!2sin!4v1728021468235!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
       </div>
         <Testimonials />
