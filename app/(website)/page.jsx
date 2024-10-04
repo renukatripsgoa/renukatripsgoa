f9ea8ec228b3c1f4a8f633251b7398477ed581d4
renukatripsgoa <renukatripsgoa44@gmail.com>
2024-10-04 11:32:25 +0530

    Update pagerrr

--- app/(website)/page.jsx
+++ app/(website)/page.jsx
@@ -25,10 +25,17 @@ const Home = () => {
         <AboutUsSection />
         <Card onSelectTitle={handleCardSelect} />
               <div className="flex h-44 w-full shadow-md rounded-xl border object-cover">
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3843.9884543128114!2d73.75903857508901!3d15.538749653420867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbfc14acb1c97c9%3A0x15975bbbeeec0083!2sRenuka%20Trips%20Goa!5e0!3m2!1sen!2sin!4v1728021468235!5m2!1sen!2sin"
+    width="600"
+    height="450"
+    style={{ border: 0 }}  // Correctly using style as an object
+    allowFullScreen=""
+    loading="lazy"
+    referrerPolicy="no-referrer-when-downgrade"
+  />
+</div>
 
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3843.9884543128114!2d73.75903857508901!3d15.538749653420867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbfc14acb1c97c9%3A0x15975bbbeeec0083!2sRenuka%20Trips%20Goa!5e0!3m2!1sen!2sin!4v1728021468235!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
-      </div>
         <Testimonials />
         <div ref={contactRef}>
           <Contact selectedService={selectedTitle} />
